8219cc2c39a989a956b17cd99788ffa91a19b186
Raymond Nguyen <rayknguyen@gmail.com>
2020-04-01 16:45:10 -0600

    Add configaration to production and development

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,15 @@
 Rails.application.configure do
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.sendgrid.net',
+    port:                 '587',
+    domain:               'example.com',
+    user_name:            ENV["SENDGRID_USERNAME"],
+    password:             ENV["SENDGRID_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
   # Verifies that versions and hashed value of the package contents in the project's package.json
   config.webpacker.check_yarn_integrity = false
   # Settings specified here will take precedence over those in config/application.rb.
